a0acf2d6947fa6edbacb37c8ac8bc4471dd17c05
Victor Wen <vicwen02@g.ucla.edu>
2021-07-14 16:02:51 -0400

    Moved full_run to a class for serialization and recovery

--- Helper.py
+++ Helper.py
@@ -7,6 +7,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import csv
 import math
+import pickle
 
 
 def load_dataset(folder):
@@ -15,20 +16,20 @@ def load_dataset(folder):
   return train, test
 
 
-def prepare_dataset(scaler, labeler, dataset):
-  y = labeler.fit_transform(dataset.pop("genre"))
-  X = scaler.fit_transform(dataset)
+def separate_variables(dataset):
+  return dataset.pop("genre"), dataset
+
+
+def prepare_dataset(scaler, labeler, X, y):
+  y = labeler.transform(y)
+  X = scaler.transform(X)
   return X, y
 
 
-def bootstrap_trials(base_model, n_trials, name, X, y, verbose=True):
+def bootstrap_trials(base_model, n_trials, X, y, verbose=True):
   models = []
-  # code for stable randomization
-  mod = int(1E6) + 7
-  code = (hash(name) ^ mod) % mod
 
   start_time = datetime.datetime.now()
-  if verbose: print("Starting")
   for i in range(n_trials):
     trial_start = datetime.datetime.now()
     trial_X, trial_y = resample(X, y, random_state=code+i)
@@ -58,7 +59,7 @@ def parent_accuracy(test_y, predict, indicators):
   return accuracy_score(parent_y, parent_predict)
 
 
-def evaluate_models(models, name, test_X, test_y, indicators, save_folder):
+def evaluate_models(models, name, test_X, test_y, indicators, save_folder=None):
   accuracies = []
   f1_scores = []
   parent_scores = []
@@ -70,7 +71,7 @@ def evaluate_models(models, name, test_X, test_y, indicators, save_folder):
     accuracies.append(accuracy)
     f1_scores.append(f1)
     parent_scores.append(parent)
-  save_to_csv(name + '.csv', [accuracies, f1_scores, parent_scores], save_folder)
+  if save_folder is not None: save_to_csv(name + '.csv', [accuracies, f1_scores, parent_scores], save_folder)
   return accuracies, f1_scores, parent_scores
 
 
@@ -91,26 +92,57 @@ def get_CIs(scores, CONFIDENCE=95):
   return np.percentile(scores, low_percentile), \
     np.median(scores), \
     np.percentile(scores, high_percentile)
+    
+    
+class ModelRunner:
+    def __init__(base_model, trials, name, data_folder, indicators, save_folder, labeler=None):
+        self.base_model = base_model
+        self.trials = trials
+        self.last_saved = 0
+        self.name = name
+        self.data_folder = data_folder
+        self.indicators = indicators
+        self.save_folder = save_folder
+        if labeler is None:
+            labeler = LabelEncoder()
+        self.labeler = labeler
+     
+        self.accuracies = []
+        self.f1_scores = []
+        self.parent_accuracies = []
+    
+    def run_models(verbose=True,save_rate=-1):
+        print(self.name)
+        
+        train, test = load_dataset(self.data_folder)
+        X, y = separate_variables(train)
+        scaler = StandardScaler()
+        scaler.fit(X)
+        if labeler is None:
+            labeler = LabelEncoder()
+            labeler.fit(y)
+        X, y = prepare_dataset(scaler, labeler, X, y)
+        X_test, y_test = separate_variables(test)
+        X_test, y_test = prepare_dataset(scaler, labeler, X_test_, y_test)
+        
+        if save_rate < 0: save_rate = trials
+        while self.last_saved < self.trials:
+            models = bootstrap_trials(self.base_model, min(self.trials-self.last_saved, save_rate), X, y, verbose)
+            a, f, p = evaluate_models(models, name, X_test, y_test, indicators)
+            self.accuracies.extend(a)
+            self.f1_scores.extend(f)
+            self.parent_accuracies.extend(p)
+            with open(save_folder + '/' + name, 'wb+') as file:
+                pickle.dump(self, file, protocol=pickle.HIGHEST_PROTOCOL)
+         
+        lo_acc, mid_acc, hi_acc = get_CIs(self.accuracies)
+        lo_f1, mid_f1, hi_f1 = get_CIs(self.f1_scores)
+        lo_p, mid_p, hi_p = get_CIs(self.parent_accuracies)
+        print("\tACCURACY:", f'{lo_acc*100:.3f} - {mid_acc*100:.3f} - {hi_acc*100:.3f}%')
+        print("\tF1 SCORES:", f'{lo_f1*100:.3f} - {mid_f1*100:.3f} - {hi_f1*100:.3f}%')
+        print("\tPARENT ACCURACIES:", f'{lo_p*100:.3f} - {mid_p*100:.3f} - {hi_p*100:.3f}%')
 
 
-def full_run(base_model, trials, name, folder, indicators, save_folder, verbose=True, save_rate=-1, labeler=None):
-  print(name)
-  train, test = load_dataset(folder)
-
-  scaler = StandardScaler()
-  if labeler is None:
-    labeler = LabelEncoder()
-  X, y = prepare_dataset(scaler, labeler, train)
-  X_test, y_test = prepare_dataset(scaler, labeler, test)
-
-  if save_rate < 0: save_rate = trials
-  models = []
-  for i in range(math.ceil(trials / save_rate)):
-    models.extend(bootstrap_trials(base_model, min(trials - save_rate * i, save_rate), name + str(i), X, y, verbose))
-    accuracies, scores, parents = evaluate_models(models, name + str(i), X_test, y_test, indicators, save_folder)
-  lo_acc, mid_acc, hi_acc = get_CIs(accuracies)
-  lo_f1, mid_f1, hi_f1 = get_CIs(scores)
-  lo_p, mid_p, hi_p = get_CIs(parents)
-  print("\tACCURACY:", f'{lo_acc*100:.3f} - {mid_acc*100:.3f} - {hi_acc*100:.3f}%')
-  print("\tF1 SCORES:", f'{lo_f1*100:.3f} - {mid_f1*100:.3f} - {hi_f1*100:.3f}%')
-  print("\tPARENT ACCURACIES:", f'{lo_p*100:.3f} - {mid_p*100:.3f} - {hi_p*100:.3f}%')
+def load_model_runner(save_folder, name):
+    with open(save_folder + '/' + name, 'rb') as file:
+        return pickle.load(file)
